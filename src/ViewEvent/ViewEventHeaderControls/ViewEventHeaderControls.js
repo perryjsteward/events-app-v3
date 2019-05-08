0e8c5b01c817c34614716d0a5816e5c22c8e9827
Perry Steward <perryjsteward@gmail.com>
2019-05-08 15:08:54 +0100

    mobile resposiveness

--- src/ViewEvent/ViewEventHeaderControls/ViewEventHeaderControls.js
+++ src/ViewEvent/ViewEventHeaderControls/ViewEventHeaderControls.js
@@ -7,8 +7,22 @@ const ViewEventHeaderControls = () => {
         <React.Fragment>
             <div className="event__controls-container">
                 <Link to='/'>
-                    <span className="fas fa-chevron-left event__view-controls"></span>
+                    <div className="event__controls-icon__container">
+                        <div className="event__controls-icon__bg"></div>
+                        <span className="fas fa-chevron-left event__controls-icon"></span>
+                    </div>   
                 </Link>
+                <div className="align-right">
+                    <div className="event__controls-icon__container">
+                        <div className="event__controls-icon__bg"></div>
+                        <span className="far fa-heart event__controls-icon"></span>
+                    </div> 
+                    <div className="spacer"></div>
+                    <div className="event__controls-icon__container">
+                        <div className="event__controls-icon__bg"></div>
+                        <span className="far fa-paper-plane event__controls-icon"></span>
+                    </div> 
+                </div>
             </div>
         </React.Fragment>
     );
