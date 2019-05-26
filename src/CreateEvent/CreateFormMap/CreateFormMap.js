3a628e79f3a4bfbd018ee479e3a8245af6a300b4
Perry Steward <perryjsteward@gmail.com>
2019-05-26 10:39:05 +0100

    let users click map to set location

--- src/CreateEvent/CreateFormMap/CreateFormMap.js
+++ src/CreateEvent/CreateFormMap/CreateFormMap.js
@@ -12,10 +12,35 @@ class CreateFormMap extends Component {
 
     state = {
         isLoading: false,
-        selectedPlace: null
+        selectedPlace: null,
+        marker: null
+    }
+
+    addressInput =  React.createRef();
+
+    componentDidMount = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position, error) => {
+                if(position) {
+                    this.setState({
+                        currentLng: position.coords.longitude,
+                        currentLat: position.coords.latitude
+                    })
+                    if(this.state.hasMapLoaded) {
+                        this.handleMapLoad();
+                    }
+                }
+                if(error) {
+                   console.log(error);
+                }
+            });
+        }
     }
 
     handleScriptLoad = () => {
+        this.setState({
+            hasMapLoaded: true
+        })
         // Declare Options For Autocomplete 
         var options = { }; // empty on purpose 
         
@@ -25,6 +50,7 @@ class CreateFormMap extends Component {
             document.getElementById('autocomplete'),
             options 
         ); 
+        this.handleMapLoad();
         // Fire Event when a suggested name is selected
         this.autocomplete.addListener('place_changed', this.handlePlaceSelect); 
     }
@@ -34,26 +60,33 @@ class CreateFormMap extends Component {
         const place = this.autocomplete.getPlace();
         let address = place.formatted_address;
         if(place.name) {
-            address = place.name + ', ' + place.formatted_address;
+            address = place.name + ', ' + place.formatted_address;  
         }
-        if(place.name){
-            selectedPlace = {
-                address: address,
-                lat: place.geometry.location.lat(),
-                lng: place.geometry.location.lng(),
-                name: place.name
-            }
+        selectedPlace = {
+            address: address,
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+            name: place.name ? place.name : address
         }
         this.setState({
             selectedPlace: selectedPlace
         });
         this.props.onSetLocation(this.state.selectedPlace);
-        this.handleMapLoad();
+        this.placeMarker({
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng()
+        });
     }
 
-    handleMapLoad = () => {
-        let lat = null;
-        let lng = null;
+    handleMapLoad = (startLat = 51.51990, startLng = 0.151876) => {
+        let lat = startLat;
+        let lng = startLng;
+
+        if(this.state.currentLng && this.state.currentLat) {
+            lat = this.state.currentLat;
+            lng = this.state.currentLng;
+        }
+
         if(this.state.selectedPlace){
             lat = this.state.selectedPlace.lat; 
             lng = this.state.selectedPlace.lng;
@@ -62,6 +95,7 @@ class CreateFormMap extends Component {
             lat: lat, 
             lng: lng, 
         };
+
         let map = new google.maps.Map(document.getElementById('map'), {
             center: location,
             zoom: 16,
@@ -69,7 +103,72 @@ class CreateFormMap extends Component {
             mapTypeControl: false,
             streetViewControl: false,
         });
-        new google.maps.Marker({position: location, map: map}); 
+
+        this.setState({
+            map: map
+        })
+
+        if(this.state.selectedPlace){
+            this.placeMarker(location, this.state.map);
+        }
+
+        map.addListener('click', (event) => {
+            if(event.latLng){
+                let location = {
+                    lat: event.latLng.lat(), 
+                    lng: event.latLng.lng(), 
+                };
+                this.placeMarker(location, true)
+            }  
+        });
+    }
+    
+    placeMarker = (location, userClicked = false) => {
+        if(this.state.marker) {
+            this.state.marker.setMap(null);
+        }
+        if(!userClicked) {
+            this.state.map.setCenter(location);
+        } else {
+            this.setSelectedLocation(location);
+        }
+        const marker = new google.maps.Marker({position: location, map: this.state.map}); 
+        this.setState({
+            marker: marker
+        });
+        this.state.marker.setMap(this.state.map);
+    }
+
+    setSelectedLocation = (location) => {
+        const geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'location': location }, (results, status) => {
+            if (status === 'OK') {
+              if (results[0]) {
+                let selectedPlace = {};
+                const place = results[0];
+                let address = place.formatted_address;
+                if(place.name) {
+                    address = place.name + ', ' + place.formatted_address;    
+                }
+                selectedPlace = {
+                    address: address,
+                    lat: place.geometry.location.lat(),
+                    lng: place.geometry.location.lng(),
+                    name: place.name ? place.name : address
+                }
+                this.addressInput.current.value = address;
+                this.setState({
+                    selectedPlace: selectedPlace
+                });
+                this.props.onSetLocation(this.state.selectedPlace);
+              } else {
+                window.alert('No results found');
+              }
+            } else {
+              window.alert('Geocoder failed due to: ' + status);
+            }
+          });
+       
     }
 
     handleUserInput = e => {}
@@ -80,11 +179,11 @@ class CreateFormMap extends Component {
                 <Script 
                     onLoad={this.handleScriptLoad} 
                     url={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}&libraries=places`}/>
-               
-                
+                   
                 <div className="map__input-field">
                     <Input 
                         id="autocomplete"
+                        ref={this.addressInput}
                         onChange={(e) => this.handleUserInput(e)} //need to smartly remove this
                         icon="fa-search"
                         size="medium"
